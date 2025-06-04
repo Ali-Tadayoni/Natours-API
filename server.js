cb0e75ade476cdd93eb9167d3c7845441c1d2b45
“Ali <tadayoni.ali.enayat@gmail.com>
2025-06-04 18:26:07 +0330

    chore: add eslint and prettier

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
 // Server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App is running on port: ${port}`);
 });
