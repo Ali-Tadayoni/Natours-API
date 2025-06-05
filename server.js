fc8c15938a0c3f8bd6ecea802be49993791adf05
“Ali <tadayoni.ali.enayat@gmail.com>
2025-06-05 16:35:56 +0330

    Add server with mongoose

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connection successful!');
+  });
 
 // Server
 const port = process.env.PORT || 3000;
