1373d15bead73fe3ea7334f2e730665ea1b17259
“Ali <tadayoni.ali.enayat@gmail.com>
2025-06-05 23:09:58 +0330

    Implement Field Limiting for API

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -13,11 +13,20 @@ exports.getAllTours = async (req, res) => {
 
     let query = Tour.find(JSON.parse(queryStr));
 
+    // 2) Sorting
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
       query = query.sort(sortBy);
     } else {
       query = query.sort('-createdAt');
+    }
+
+    // 3) Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
     }
 
     const tours = await query;
